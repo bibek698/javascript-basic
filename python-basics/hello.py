561ef23c8c01db0cbd583dad2e96de7c806415b3
bibek698 <mahat6981@gmail.com>
2026-08-06 14:01:58 +0545

    added arithmetic opreators

--- python-basics/hello.py
+++ python-basics/hello.py
@@ -1,1 +1,18 @@
 print("hello world")
+
+# Multiplication, division, addition, and subtraction
+print(3*8/2+5-1)
+ 
+# Exponents
+print(4**6) # Syntax means 4 to the power of 6
+print(4**2) # To square a number
+print(4**3) # To cube a number
+print(4**0.5) # To find the square root of a number
+
+# To calculate how many different possible combinations can be
+# formed using a set of "x" characters with each character in "x"
+# having "y" number of possible values, you will need to use an 
+# exponent for the calculation:
+x = 2
+y = 2
+print(y**x)
