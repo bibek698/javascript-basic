3528a2dda4d611dd821b1633a96656eb7923ad6e
bibek698 <mahat6981@gmail.com>
2026-08-07 20:04:02 +0545

    refactor hello.py

--- python-basics/hello.py
+++ python-basics/hello.py
@@ -1,10 +1,10 @@
 print("hello world")
 
-print(7+8.5)
+print(7+8.5) #implicit conversion: converting one data type to another data type
 print("a"+"b"+"c")
 print("This " + "is " + "pretty " + "neat!")
 
 base = 6
 height = 3
 area = (base*height)/2
-print("The area of the triangle is: " + str(area)) 
+print("The area of the triangle is: " + str(area)) #explicit conversion: converted integer to string
