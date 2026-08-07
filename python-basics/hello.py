39a60c6178029fd682b813dabdf40f3e27df66ee
bibek698 <mahat6981@gmail.com>
2026-08-07 20:00:30 +0545

    added python expressions

--- python-basics/hello.py
+++ python-basics/hello.py
@@ -1,1 +1,10 @@
 print("hello world")
+
+print(7+8.5)
+print("a"+"b"+"c")
+print("This " + "is " + "pretty " + "neat!")
+
+base = 6
+height = 3
+area = (base*height)/2
+print("The area of the triangle is: " + str(area)) 
